5a9d3a2f19a2b42cdb80df5095502cd9fe10bbc2
Raffaella Rinaldi <74203968+raffaellarinaldi@users.noreply.github.com>
2023-06-17 14:30:58 +0200

    Update site.js

--- src/data/site.js
+++ src/data/site.js
@@ -15,6 +15,7 @@ module.exports = () => {
       facebook: 'https://www.facebook.com/Agenzia-immobiliare-Valcalepio-551357054962575/',
       instagram: '#',
       linkedin: 'https://www.linkedin.com/in/sergio-manenti-b7214855/',
+      youtube: 'https://youtube.com/@immobiliarevalcalepio',
       google: 'https://g.page/agenzia-immobiliare-valcalepio?share'
     }
   };
