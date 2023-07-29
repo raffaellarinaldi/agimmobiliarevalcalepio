7c683e2c8b35a66e3136995772ce95c06f8aedc4
Raffaella <raffaellarinaldiweb@gmail.com>
2023-07-29 19:20:20 +0000

    Test

--- src/data/properties.js
+++ src/data/properties.js
@@ -1,22 +1,58 @@
-const cheerio = require('cheerio');
-const EleventyFetch = require("@11ty/eleventy-fetch");
+// Source: https://dev.to/diass_le/tutorial-web-scraping-with-nodejs-and-cheerio-2jbh
 
-const fetchHtml = async (url) => {
-  return EleventyFetch(url, {
-    duration: '1d', // Cache duration set to 1 day
-    type: 'text', // Cache response as text
-  });
+const
+cheerio = require('cheerio'),
+axios = require('axios').default;
+
+const fethHtml = async url => {
+  try {
+    const { data } = await axios.get(url);
+    return data;
+  } catch {
+    console.error(`ERROR: An error occurred while trying to fetch the URL: ${url}`);
+  }
 };
 
-const extractAd = ($, selector) => {
-  const title = $(selector).find('.disp-tit > a').html().split('<br>')[0].trim();
-  const url = $(selector).find('.disp-tit > a').attr('href');
-  const image = $(selector).find('.homere').attr('src');
-  const city = $(selector).find('.disp-tit > a').html().split('<br>')[1].trim();
-  const price = $(selector).find('.disp-foot').text().trim();
-  const bed = $(selector).find('.disp-dat b:nth-of-type(1)').text().trim();
-  const bath = $(selector).find('.disp-dat b:nth-of-type(2)').text().trim();
-  const sqm = $(selector).find('.disp-dat b:nth-of-type(3)').text().trim();
+const extractAd = selector => {
+  const title = selector
+  .find('.disp-tit > a')
+  .html()
+  .split('<br>')[0]
+  .trim()
+
+  const url = selector
+  .find('.disp-tit > a')
+  .attr('href')
+
+  const image = selector
+  .find('.homere')
+  .attr('src')
+
+  const city = selector
+  .find('.disp-tit > a')
+  .html()
+  .split('<br>')[1]
+  .trim()
+
+  const price = selector
+  .find('.disp-foot')
+  .text()
+  .trim()
+
+  const bed = selector
+  .find('.disp-dat b:nth-of-type(1)')
+  .text()
+  .trim()
+
+  const bath = selector
+  .find('.disp-dat b:nth-of-type(2)')
+  .text()
+  .trim()
+
+  const sqm = selector
+  .find('.disp-dat b:nth-of-type(3)')
+  .text()
+  .trim()
 
   return {
     title,
@@ -26,21 +62,28 @@ const extractAd = ($, selector) => {
     price,
     sqm,
     bed,
-    bath,
-  };
-};
+    bath
+  }
+}
 
 const scrapAvc = async () => {
-  const avcUrl = 'https://www.agenziavalcalepio.it/';
+  const avcUrl =
+  'https://www.agenziavalcalepio.it/';
 
-  const html = await fetchHtml(avcUrl);
-  const $ = cheerio.load(html);
+  const html = await fethHtml(avcUrl);
 
-  const searchResults = $('.dispgrp > .dispbox');
+  const selector = cheerio.load(html);
+
+  const searchResults = selector('body').find(
+    '.dispgrp > .dispbox'
+  );
 
   const ads = searchResults
-    .map((idx, el) => extractAd($, el))
-    .get();
+  .map((idx, el) => {
+    const elementSelector = selector(el);
+    return extractAd(elementSelector);
+  })
+  .get();
 
   console.log(ads);
 
@@ -48,97 +91,3 @@ const scrapAvc = async () => {
 };
 
 module.exports = scrapAvc;
-
-// Source: https://dev.to/diass_le/tutorial-web-scraping-with-nodejs-and-cheerio-2jbh
-
-// const
-// cheerio = require('cheerio'),
-// axios = require('axios').default;
-
-// const fethHtml = async url => {
-//   try {
-//     const { data } = await axios.get(url);
-//     return data;
-//   } catch {
-//     console.error(`ERROR: An error occurred while trying to fetch the URL: ${url}`);
-//   }
-// };
-
-// const extractAd = selector => {
-//   const title = selector
-//   .find('.disp-tit > a')
-//   .html()
-//   .split('<br>')[0]
-//   .trim()
-
-//   const url = selector
-//   .find('.disp-tit > a')
-//   .attr('href')
-
-//   const image = selector
-//   .find('.homere')
-//   .attr('src')
-
-//   const city = selector
-//   .find('.disp-tit > a')
-//   .html()
-//   .split('<br>')[1]
-//   .trim()
-
-//   const price = selector
-//   .find('.disp-foot')
-//   .text()
-//   .trim()
-
-//   const bed = selector
-//   .find('.disp-dat b:nth-of-type(1)')
-//   .text()
-//   .trim()
-
-//   const bath = selector
-//   .find('.disp-dat b:nth-of-type(2)')
-//   .text()
-//   .trim()
-
-//   const sqm = selector
-//   .find('.disp-dat b:nth-of-type(3)')
-//   .text()
-//   .trim()
-
-//   return {
-//     title,
-//     url,
-//     image,
-//     city,
-//     price,
-//     sqm,
-//     bed,
-//     bath
-//   }
-// }
-
-// const scrapAvc = async () => {
-//   const avcUrl =
-//   'https://www.agenziavalcalepio.it/';
-
-//   const html = await fethHtml(avcUrl);
-
-//   const selector = cheerio.load(html);
-
-//   const searchResults = selector('body').find(
-//     '.dispgrp > .dispbox'
-//   );
-
-//   const ads = searchResults
-//   .map((idx, el) => {
-//     const elementSelector = selector(el);
-//     return extractAd(elementSelector);
-//   })
-//   .get();
-
-//   console.log(ads);
-
-//   return ads;
-// };
-
-// module.exports = scrapAvc;
