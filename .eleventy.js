057ee3d0d65ac67c79ab93bfbaa3650b67c8d22e
Raffaella <raffaellarinaldiweb@gmail.com>
2023-08-09 13:16:45 +0000

    Bugfixes!

--- .eleventy.js
+++ .eleventy.js
@@ -11,7 +11,7 @@ module.exports = (eleventyConfig) => {
   // })
   // Copied for bugfixing purposes, workaround
     eleventyConfig.setTemplateFormats('html,liquid,njk')
-    eleventyConfig.addTransform('minifyHTML', require('./libraries/minifier'))
+    //eleventyConfig.addTransform('minifyHTML', require('./libraries/minifier'))
     eleventyConfig.addShortcode('date', () => `${new Date().toISOString().slice(0, 10)}`)
     eleventyConfig.addNunjucksShortcode('year', () => `${new Date().getFullYear()}`)
     eleventyConfig.addShortcode('11ty_version', () => require('@11ty/eleventy/package.json').version)
