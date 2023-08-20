84ddc23b9dc6d595e609dc2caabddbf6b9925c67
Raffaella <raffaellarinaldiweb@gmail.com>
2023-08-20 13:45:07 +0000

    Bugfixes

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 const
 { EleventyServerlessBundlerPlugin } = require('@11ty/eleventy'),
-//Copied for bugfixing purposes
-htmlmin = require('html-minifier').minify
+htmlmin = require('html-minifier').minify;
 
 module.exports = (eleventyConfig) => {
   //eleventyConfig.setQuietMode(true)
@@ -10,21 +9,21 @@ module.exports = (eleventyConfig) => {
     './src/static': './'
   })
   // Copied for bugfixing purposes, workaround
-    eleventyConfig.setTemplateFormats('html,liquid,njk')
-    eleventyConfig.addShortcode('date', () => `${new Date().toISOString().slice(0, 10)}`)
-    eleventyConfig.addNunjucksShortcode('year', () => `${new Date().getFullYear()}`)
-    eleventyConfig.addShortcode('11ty_version', () => require('@11ty/eleventy/package.json').version)
-    module.exports = (content, outputPath) => {
-	    if (outputPath && outputPath.endsWith('.html') && process.env.ELEVENTY_ENV !== 'development') {
-		    return htmlmin(content, {
-			    collapseWhitespace: true,
-			    conservativeCollapse: true,
-			    removeComments: true,
-			    useShortDoctype: true
-		    })
-	   }
+  eleventyConfig.setTemplateFormats('html,liquid,njk')
+  eleventyConfig.addShortcode('date', () => `${new Date().toISOString().slice(0, 10)}`)
+  eleventyConfig.addNunjucksShortcode('year', () => `${new Date().getFullYear()}`)
+  eleventyConfig.addShortcode('11ty_version', () => require('@11ty/eleventy/package.json').version)
+  eleventyConfig.addTransform('minify', (content, outputPath) => {
+    if (outputPath && outputPath.endsWith('.html') && process.env.ELEVENTY_ENV !== 'development') {
+		  return htmlmin(content, {
+			  collapseWhitespace: true,
+			  conservativeCollapse: true,
+			  removeComments: true,
+			  useShortDoctype: true
+		  })
+	  }
 	  return content
-  }  
+  })
   // Endcopied
   eleventyConfig.addLayoutAlias('base', '../layouts/base.njk')
   eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
