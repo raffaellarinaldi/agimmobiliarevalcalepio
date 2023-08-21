6e43802ad71d67fd89cad289913b8b68fd7e7cbf
Raffaella <raffaellarinaldiweb@gmail.com>
2023-08-21 08:49:16 +0000

    Restored

--- .eleventy.js
+++ .eleventy.js
@@ -4,23 +4,23 @@ htmlmin = require('html-minifier').minify
 
 module.exports = (eleventyConfig) => {
   //eleventyConfig.setQuietMode(true)
-  require('./.eleventy.main')(eleventyConfig)
+  //require('./.eleventy.main')(eleventyConfig)
   // Copied for bugfixing purposes, workaround
-  // eleventyConfig.setTemplateFormats('html,liquid,njk')
-  // eleventyConfig.addShortcode('date', () => `${new Date().toISOString().slice(0, 10)}`)
-  // eleventyConfig.addNunjucksShortcode('year', () => `${new Date().getFullYear()}`)
-  // eleventyConfig.addShortcode('11ty_version', () => require('@11ty/eleventy/package.json').version)
-  // eleventyConfig.addTransform('minify', (content, outputPath) => {
-  //   if (outputPath && outputPath.endsWith('.html') && process.env.ELEVENTY_ENV !== 'development') {
-		//   return htmlmin(content, {
-		// 	  collapseWhitespace: true,
-		// 	  conservativeCollapse: true,
-		// 	  removeComments: true,
-		// 	  useShortDoctype: true
-		//   })
-	 // }
-	 // return content
-  // })
+  eleventyConfig.setTemplateFormats('html,liquid,njk')
+  eleventyConfig.addShortcode('date', () => `${new Date().toISOString().slice(0, 10)}`)
+  eleventyConfig.addNunjucksShortcode('year', () => `${new Date().getFullYear()}`)
+  eleventyConfig.addShortcode('11ty_version', () => require('@11ty/eleventy/package.json').version)
+  eleventyConfig.addTransform('minify', (content, outputPath) => {
+    if (outputPath && outputPath.endsWith('.html') && process.env.ELEVENTY_ENV !== 'development') {
+		  return htmlmin(content, {
+			  collapseWhitespace: true,
+			  conservativeCollapse: true,
+			  removeComments: true,
+			  useShortDoctype: true
+		  })
+	  }
+	  return content
+  })
   // Endcopied
   eleventyConfig.addPassthroughCopy({
     './src/static': './'
