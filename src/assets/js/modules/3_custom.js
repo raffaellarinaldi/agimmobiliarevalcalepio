ea690f6c5b0082bef9e08cc70e5f004f88f7fdd8
Raffaella Rinaldi <74203968+raffaellarinaldi@users.noreply.github.com>
2025-01-23 14:36:19 +0100

    Update 3_custom.js

--- src/assets/js/modules/3_custom.js
+++ src/assets/js/modules/3_custom.js
@@ -1,4 +1,5 @@
 (function () {
+
   'use strict'
 
   AOS.init({
@@ -7,14 +8,15 @@
     once: true
   })
 
-  var preloader = function() {
+  var preloader = function () {
+
     var loader = document.querySelector('.loader')
     var overlay = document.getElementById('overlayer')
 
     function fadeOut(el) {
       el.style.opacity = 1
-      ;(function fade() {
-        if ((el.style.opacity -= 0.1) < 0) {
+      (function fade() {
+        if ((el.style.opacity -= .1) < 0) {
           el.style.display = 'none'
         } else {
           requestAnimationFrame(fade)
@@ -22,21 +24,22 @@
       })()
     }
 
-    setTimeout(function() {
+    setTimeout(function () {
       fadeOut(loader)
       fadeOut(overlay)
     }, 200)
   }
   preloader()
 
-  var tinySlider = function() {
+  var tinySdlier = function () {
+
     var heroSlider = document.querySelectorAll('.hero-slide')
     var propertySlider = document.querySelectorAll('.property-slider')
     var imgPropertySlider = document.querySelectorAll('.img-property-slide')
     var testimonialSlider = document.querySelectorAll('.testimonial-slider')
 
     if (heroSlider.length > 0) {
-      tns({
+      var tnsHeroSlider = tns({
         container: '.hero-slide',
         mode: 'carousel',
         speed: 700,
@@ -49,7 +52,7 @@
     }
 
     if (imgPropertySlider.length > 0) {
-      tns({
+      var tnsPropertyImageSlider = tns({
         container: '.img-property-slide',
         mode: 'carousel',
         speed: 700,
@@ -63,7 +66,7 @@
     }
 
     if (propertySlider.length > 0) {
-      tns({
+      var tnsSlider = tns({
         container: '.property-slider',
         mode: 'carousel',
         speed: 700,
@@ -73,15 +76,21 @@
         autoplayButtonOutput: false,
         controlsContainer: '#property-nav',
         responsive: {
-          0: { items: 1 },
-          700: { items: 2 },
-          900: { items: 3 }
+          0: {
+            items: 1
+          },
+          700: {
+            items: 2
+          },
+          900: {
+            items: 3
+          }
         }
       })
     }
 
     if (testimonialSlider.length > 0) {
-      tns({
+      var tnsSlider = tns({
         container: '.testimonial-slider',
         mode: 'carousel',
         speed: 700,
@@ -91,12 +100,19 @@
         autoplayButtonOutput: false,
         controlsContainer: '#testimonial-nav',
         responsive: {
-          0: { items: 1 },
-          700: { items: 2 },
-          900: { items: 3 }
+          0: {
+            items: 1
+          },
+          700: {
+            items: 2
+          },
+          900: {
+            items: 3
+          }
         }
       })
     }
   }
-  tinySlider()
+  tinySdlier()
+
 })()
